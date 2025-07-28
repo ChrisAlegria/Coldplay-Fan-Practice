1fd46b59e4e7154fac19b1d37d08127a00c2ddef
Chris Alegria <christian.alegriar@gmail.com>
2025-07-27 22:42:22 -0600

    Update: Styles On Dropdown

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,13 @@
 // ! width 899px Script.
 let screenWidth = window.innerWidth;
 
+// ~Width Principal Functions
 if(screenWidth > 800){
-    document.getElementById('navegation-hidden-option').style = 'display:none'
+    document.getElementById('navegation-hidden-option').style = 'display:none';
+    document.getElementById('')
 
 } else {
-    document.getElementById('navegation-hidden-option').style = 'display:block'
+    document.getElementById('navegation-hidden-option').style = 'display:block';
 
 }
 
@@ -18,12 +20,12 @@ function navegationMenuDropdown(){
         document.getElementsByTagName('nav')[0].style = 'display: block';
         document.getElementsByTagName('main')[0].style = 'display: none';
         document.getElementsByTagName('footer')[0].style = 'display: none';
-        document.getElementById('navegation-menu-element').textContent = 'CLOSE'
+        document.getElementById('navegation-menu-element').textContent = 'CLOSE';
         return
 
     } else if(navegationDropdownActivation == 'CLOSE'){
+        document.getElementById('navegation-menu-element').textContent = 'MENU';
         document.getElementsByTagName('nav')[0].style = 'display: none';
-        document.getElementById('navegation-menu-element').textContent = 'MENU'
         document.getElementsByTagName('main')[0].style = 'display: block';
         document.getElementsByTagName('footer')[0].style = 'display: block';
         return
