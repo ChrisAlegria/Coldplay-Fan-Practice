6476aa3ab524f46d57764da743746eff74840793
Chris Alegria <christian.alegriar@gmail.com>
2025-07-30 16:38:35 -0600

    Fix: Dropdawn Menu Js Functions

--- scripts/script.js
+++ scripts/script.js
@@ -17,10 +17,13 @@ function navegationMenuDropdown(){
     let navegationDropdownActivation = document.getElementById('navegation-menu-element').textContent;
 
     if(navegationDropdownActivation == 'MENU'){
-        document.getElementsByTagName('nav')[0].style = 'display: block';
+        document.getElementsByTagName('nav')[0].style = 'display: block; transition: display 2s;';
         document.getElementsByTagName('main')[0].style = 'display: none';
         document.getElementsByTagName('footer')[0].style = 'display: none';
         document.getElementById('navegation-menu-element').textContent = 'CLOSE';
+        document.getElementsByClassName('media-branding-nav')[0].style = 'margin-left: 1.5vw';
+        document.getElementsByClassName('media-branding-nav')[1].style = 'margin-right: 1.5vw';   
+        document.getElementById('moons-logo').style = 'margin-left: 0.1vw';
         return
 
     } else if(navegationDropdownActivation == 'CLOSE'){
