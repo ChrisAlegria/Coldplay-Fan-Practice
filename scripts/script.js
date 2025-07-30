757b3bdacf47a2feb331ceb48c3ae5c1d8f2593a
Chris Alegria <christian.alegriar@gmail.com>
2025-07-30 17:09:53 -0600

    Add: Animations At Navegation Dropdawn

--- scripts/script.js
+++ scripts/script.js
@@ -17,20 +17,21 @@ function navegationMenuDropdown(){
     let navegationDropdownActivation = document.getElementById('navegation-menu-element').textContent;
 
     if(navegationDropdownActivation == 'MENU'){
-        document.getElementsByTagName('nav')[0].style = 'display: block; transition: display 2s;';
+        document.getElementsByTagName('nav')[0].style = 'visibility: visible; opacity: 1;';
         document.getElementsByTagName('main')[0].style = 'display: none';
         document.getElementsByTagName('footer')[0].style = 'display: none';
-        document.getElementById('navegation-menu-element').textContent = 'CLOSE';
         document.getElementsByClassName('media-branding-nav')[0].style = 'margin-left: 1.5vw';
         document.getElementsByClassName('media-branding-nav')[1].style = 'margin-right: 1.5vw';   
-        document.getElementById('moons-logo').style = 'margin-left: 0.1vw';
+        document.getElementById('navegation-menu-element').textContent = 'CLOSE';
         return
 
     } else if(navegationDropdownActivation == 'CLOSE'){
         document.getElementById('navegation-menu-element').textContent = 'MENU';
-        document.getElementsByTagName('nav')[0].style = 'display: none';
+        document.getElementsByTagName('nav')[0].style = 'visibility: hidden; opacity: 0;';
         document.getElementsByTagName('main')[0].style = 'display: block';
         document.getElementsByTagName('footer')[0].style = 'display: block';
+        document.getElementsByClassName('media-branding-nav')[0].style = 'margin-left: 1.65vw';
+        document.getElementsByClassName('media-branding-nav')[1].style = 'margin-right: 1.65vw';   
         return
     }
 }    
