b34c88a2fb6bf9618783068194f9af7f36a690fc
Chris Alegria <christian.alegriar@gmail.com>
2025-08-06 19:12:16 -0600

    Test: General Dinamic Styles

--- scripts/script.js
+++ scripts/script.js
@@ -19,8 +19,8 @@ function navegationMenuDropdown(){
 
     if(navegationDropdownActivation == 'MENU'){
         document.getElementsByTagName('nav')[0].style = 'visibility: visible; opacity: 1;';
-        document.getElementsByTagName('main')[0].style = 'visibility: hidden; opacity: 0;';
-        document.getElementsByTagName('footer')[0].style = 'visibility: hidden; opacity: 0;';
+        document.getElementsByTagName('main')[0].style = 'visibility: hidden; opacity: 0; display: none;';
+        document.getElementsByTagName('footer')[0].style = 'visibility: hidden; opacity: 0; display: none;';
         document.getElementsByClassName('media-branding-nav')[0].style = 'margin-left: 1.5vw';
         document.getElementsByClassName('media-branding-nav')[1].style = 'margin-right: 1.5vw';   
         document.getElementById('navegation-menu-element').textContent = 'CLOSE';
