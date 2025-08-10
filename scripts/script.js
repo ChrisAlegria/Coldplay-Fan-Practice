c8c83284f8f761683a30d95b865c88d0eb79f367
Chris Alegria <christian.alegriar@gmail.com>
2025-08-10 00:05:43 -0600

    Fix: Time Animations At Dropdown

--- scripts/script.js
+++ scripts/script.js
@@ -19,8 +19,8 @@ function navegationMenuDropdown(){
 
     if(navegationDropdownActivation == 'MENU'){
         document.getElementsByTagName('nav')[0].style = 'visibility: visible; opacity: 1;';
-        document.getElementsByTagName('main')[0].style = 'visibility: hidden; opacity: 0; display: none;';
-        document.getElementsByTagName('footer')[0].style = 'visibility: hidden; opacity: 0; display: none;';
+        document.getElementsByTagName('main')[0].style = 'visibility: hidden; opacity: 0; display: none';
+        document.getElementsByTagName('footer')[0].style = 'visibility: hidden; opacity: 0; display: none';
         document.getElementsByClassName('media-branding-nav')[0].style = 'margin-left: 1.5vw';
         document.getElementsByClassName('media-branding-nav')[1].style = 'margin-right: 1.5vw';   
         document.getElementById('navegation-menu-element').textContent = 'CLOSE';
@@ -29,8 +29,8 @@ function navegationMenuDropdown(){
     } else if(navegationDropdownActivation == 'CLOSE'){
         document.getElementById('navegation-menu-element').textContent = 'MENU';
         document.getElementsByTagName('nav')[0].style = 'visibility: hidden; opacity: 0;';
-        document.getElementsByTagName('main')[0].style = 'visibility: visible; opacity: 1;';
-        document.getElementsByTagName('footer')[0].style = 'visibility: visible; opacity: 1;';
+        document.getElementsByTagName('main')[0].style = 'visibility: visible; opacity: 1; display: block';
+        document.getElementsByTagName('footer')[0].style = 'visibility: visible; opacity: 1; display: block';
         document.getElementsByClassName('media-branding-nav')[0].style = 'margin-left: 1.65vw';
         document.getElementsByClassName('media-branding-nav')[1].style = 'margin-right: 1.65vw';   
         return
